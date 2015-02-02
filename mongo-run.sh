846e132ca9a3b6da8be1bf7482a31a65e0332ba7
Karoly Negyesi <chx1975@gmail.com>
2015-02-02 07:32:11 +0100

    update to latest patch

--- mongo-run.sh
+++ mongo-run.sh
@@ -1,6 +1,7 @@
 DCI_TESTGROUPS="node" \
 DCI_DRUPALBRANCH="8.0.x"  \
-DCI_PATCH=https://www.drupal.org/files/issues/2381763_25.patch \
+DCI_PATCH="https://www.drupal.org/files/issues/2395901_9.patch" \
 DCI_DBTYPE=mongodb \
 DCI_UPDATEREPO=true \
+DCI_CONCURRENCY=7 \
 ./run.sh
